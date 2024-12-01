74b95dac7aacdc945e5faad2c00710d89af535e2
abc123 <mohtasimbillah62@gmail.com>
2024-11-30 21:29:47 -0500

    leetcode 1752 done

---
+++ 1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,11 @@
+def check(self, nums: List[int]) -> bool:
+    count = 0
+
+    for i in range(0, len(nums)-1):         # this loop iterates over each number and finds the INCREASE
+        if nums[i] > nums[i+1]:
+            count+=1
+
+    if nums[len(nums)-1] > nums[0]:         # this checks for the INCREASE in the rotation
+        count+=1
+        
+    return count<=1                         # there can be only ONE INCREASE
