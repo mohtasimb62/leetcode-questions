bf2bde74f525738f1f862ce1843800773c8024c8
abc123 <mohtasimbillah62@gmail.com>
2024-12-28 16:50:08 -0500

    optimal approach for leetcode 268

--- 268-missing-number.py
+++ 268-missing-number.py
@@ -3,7 +3,8 @@ Brute Force:
     TC: O(n^2)
     SC: O(1)
 
-    Just check whether or not each element in nums exist between 0 - len(nums)+1 (+1 to account for the last number)
+    Just check whether or not each element in nums exist between 0 - len(nums)+1 (+1 to account for the 
+    last number)
 '''
 def missingNumber(self, nums: List[int]) -> int:
     for i in range(0, len(nums)+1):
@@ -16,7 +17,8 @@ Better approach:
     TC: O(n),
     SC: O(n)
 
-    By hashing the appearance of each element in nums in a list, it is easy to tell which element is missing
+    By hashing the appearance of each element in nums in a list, it is easy to tell which element is 
+    missing
 '''
 
 def missingNumber(self, nums: List[int]) -> int:
@@ -28,3 +30,22 @@ def missingNumber(self, nums: List[int]) -> int:
     for i in range(0, len(hashArr)):
         if hashArr[i] == 0:
             return i
+
+
+'''
+Optimal approach:
+    TC: O(n),
+    SC: O(1)
+
+    If you calculate the sum using Gauss Sum (n*(n+1)//2) and calculate the sum of all the elements in 
+    nums, and get the difference, you will get the missing number
+'''
+
+def missingNumber(self, nums: List[int]) -> int:
+    n = len(nums)
+    sumOfN = (n*(n+1)) // 2 # to get an integer instead of a floating point number
+    sumOfNums = 0
+    for num in nums:
+        sumOfNums += num
+
+    return sumOfN - sumOfNums
