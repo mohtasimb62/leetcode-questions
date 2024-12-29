bf40a31eb8a5ff5a8bc80a30f7062a3236c4a231
abc123 <mohtasimbillah62@gmail.com>
2024-12-29 12:48:13 -0500

    brute force and solution with extra space for leetcode 136

---
+++ 136-single-number.py
@@ -0,0 +1,42 @@
+'''
+Brute Force:
+    TC: O(n^2),
+    SC: O(1)
+
+    For every element, check if it appears again. Return the element that appears once.
+'''
+def singleNumber(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+
+    for i in range(0, len(nums)):
+        count = 0   # to track if an element appears more than once
+        for j in range(0, len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+
+        if count == 1:
+            return nums[i]
+
+'''
+Using extra space (set):
+    TC: O(n),
+    SC: O(n)
+
+    Add every element to the set and if that element appears again, remove it from the set. Return the element
+    that remains in the set.
+'''
+def singleNumber(self, nums: List[int]) -> int:
+    hashset = set()
+
+    for num in nums:
+        if num in hashset:
+            hashset.remove(num)
+        else:
+            hashset.add(num)
+
+    singleNumber = 0
+    for item in hashset:
+        singleNumber = item
+        
+    return singleNumber
