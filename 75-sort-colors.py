5ed7a1eee25430ec5d5628491b6bb7d4d3b05908
abc123 <mohtasimbillah62@gmail.com>
2024-12-31 23:40:21 -0500

    optimal approach for leetcode 75

--- 75-sort-colors.py
+++ 75-sort-colors.py
@@ -29,3 +29,40 @@ def sortColors(self, nums: List[int]) -> None:
 
     for i in range(0, len(nums)):
         nums[i] = newArr[i]
+
+
+'''
+Optimal approach (Dutch National Flag):
+    TC: O(n),
+    SC: O(1)
+'''
+
+def sortColors(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            # swapping nums[low] and nums[mid]
+            tmp = nums[low]
+            nums[low] = nums[mid]
+            nums[mid] = tmp
+
+            # update pointers
+            low += 1
+            mid += 1
+
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            # swapping nums[mid] and nums[high]
+            tmp = nums[mid]
+            nums[mid] = nums[high]
+            nums[high] = tmp
+
+            # update pointers
+            high -= 1
