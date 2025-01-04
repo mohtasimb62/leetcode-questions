9e02c49edf70dfdb002e99c8e492ba50f4597743
abc123 <mohtasimbillah62@gmail.com>
2025-01-04 14:23:35 -0500

    better approach for leetcode 560

--- 560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k.py
@@ -1,3 +1,11 @@
+'''
+Brute force approach:
+    TC: O(n^3),
+    SC: O(1)
+
+    Generate all the subarrays and for each subarray, calculate the sum. If the sum is equal to k, increment 
+    a count variable and return that at the end.
+'''
 def subarraySum(self, nums: List[int], k: int) -> int:
     count = 0
     for i in range(0, len(nums)):
@@ -6,6 +14,26 @@ def subarraySum(self, nums: List[int], k: int) -> int:
 
             for l in range(i, j+1):
                 sum += nums[l]
+
+            if sum == k:
+                count += 1
+
+    return count
+
+'''
+Better approach:
+    TC: O(n^2),
+    SC: O(1)
+
+    Instead of calculating the sum every single time, keep track of the sum in a variable (The sum doesn't have 
+    to be calculated every iteration. It's just the whatever the sum was plus the current element.)
+'''
+def subarraySum(self, nums: List[int], k: int) -> int:
+    count = 0
+    for i in range(0, len(nums)):
+        sum = 0
+        for j in range(i, len(nums)):
+            sum += nums[j]
 
             if sum == k:
                 count += 1
