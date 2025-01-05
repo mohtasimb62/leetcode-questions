928e08bb19fb6b75173af601722121263883af73
abc123 <mohtasimbillah62@gmail.com>
2025-01-05 18:33:05 -0500

    optimal approach for leetcode 54

---
+++ 54-spiral-matrix.py
@@ -0,0 +1,45 @@
+'''
+No brute-better-optimal approach for this problem. Just have to implement it in the most cleanest way.
+    TC: O(n*m),
+    SC: O(n*m) for the result list
+'''
+
+def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    n = len(matrix)
+    m = len(matrix[0])
+    ans = []
+
+    # 4 pointers - they are determined by the "edges" or "border" of the matrix
+    left = 0
+    right = m - 1
+    top = 0
+    bottom = n - 1
+
+    while (left <= right and top <= bottom):
+        # right
+        for i in range(left, right+1):
+            ans.append(matrix[top][i])
+        
+        top += 1
+
+        # bottom
+        for i in range(top, bottom+1):
+            ans.append(matrix[i][right])
+
+        right -= 1
+    
+        # left
+        if top <= bottom:
+            for i in range(right, left-1, -1):
+                ans.append(matrix[bottom][i])
+
+            bottom -= 1
+
+        # top
+        if left <= right:
+            for i in range(bottom, top-1, -1):
+                ans.append(matrix[i][left])
+
+            left += 1
+
+    return ans
