026f656b0ddb704ec26f997d0af9c272a4657423
abc123 <mohtasimbillah62@gmail.com>
2025-01-06 13:14:39 -0500

    solution for leetcode 704

---
+++ binary-search/704-binary-search.py
@@ -0,0 +1,19 @@
+def search(self, nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        # this is for the OVERFLOW case. If both low and high are INT_MAX, then adding them will cause them to 
+        # overflow. Using the formula below will keep that in check. Or using something like "long long" in C++.
+        # mid = low + ((high - low) // 2)
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
