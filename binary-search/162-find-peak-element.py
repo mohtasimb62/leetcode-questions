3b544ef640f691d911a498400d62b373d659e1b2
abc123 <mohtasimbillah62@gmail.com>
2025-01-17 23:54:50 -0500

    optimal approach for leetcode 162

--- binary-search/162-find-peak-element.py
+++ binary-search/162-find-peak-element.py
@@ -31,4 +31,31 @@ def findPeakElement(self, nums: List[int]) -> int:
 Optimal approach (using binary search):
     TC: O(logn),
     SC: O(1)
+
+    Just like the brute force, handle the edge cases carefully. And instead of doing a linear search, do
+    a binary search and just eliminate the opposite side where the peak may lie.
 '''
+def findPeakElement(self, nums: List[int]) -> int:
+    if len(nums) == 1:
+        return 0
+
+    if nums[0] > nums[1]:
+        return 0
+
+    if nums[len(nums)-1] > nums[len(nums)-2]:
+        return len(nums)-1
+
+    low = 1
+    high = len(nums) - 2
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+            return mid
+        elif nums[mid] > nums[mid-1]:   # meaning you are on the left side of the peak
+            low = mid + 1
+        else:                           # and this means you are on the right side of the peak
+            high = mid - 1
+
+    return -1
