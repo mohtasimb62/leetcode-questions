df093eef5c8301439d5a4a286603e3daa2ca440a
abc123 <mohtasimbillah62@gmail.com>
2025-01-23 14:06:05 -0500

    optimal approach for leetcode 1283

--- binary-search/1283-find-the-smallest-divisor-given-a-threshold.py
+++ binary-search/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -6,9 +6,12 @@ Brute force:
     TC: O(max(n)) * O(n)
     SC: O(1)
 
-    Similar to leetcode 875, find the starting and ending index which will act as the divisor. Calculate
-    the sum for each divisor and as soon as you find the divisor which gives a sum that is less than or
-    equal to the threshold, return that divisor.
+    Similar to leetcode 875, find the starting and ending index which will act as the divisor. Note that
+    the ending index here is the highest element in the list. Why? Because after the highest element, the
+    result of the sum is the same, no matter how high you go.
+    
+    Calculate the sum for each divisor and as soon as you find the divisor which gives a sum that is less 
+    than or equal to the threshold, return that divisor.
 '''
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
@@ -23,6 +26,42 @@ class Solution:
                 return ans
 
         return -1
+
+    def calculateSum(self, arr, divisor):
+        res = 0
+        for i in range(0, len(arr)):
+            res += math.ceil(arr[i] / divisor)
+
+        return res
+    
+
+'''
+Optimal approach:
+    TC: O(max(n)) * O(logn)
+    SC: O(1)
+
+    Just like the brute force, but instead of doing a linear iteration of calculating the sum for each
+    divisor, do a binary search to trim down the search space. Return as soon as you find the divisor
+    that is less than or equal to the threshold.
+'''
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        low = 1
+        high = max(nums)
+        ans = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            currSum = self.calculateSum(nums, mid)
+            
+            if currSum <= threshold:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
 
     def calculateSum(self, arr, divisor):
         res = 0
