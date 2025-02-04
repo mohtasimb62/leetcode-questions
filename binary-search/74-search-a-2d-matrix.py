fd84669ae5c1bd9271b67ad7e8b3b4bf0b76c6f4
abc123 <mohtasimbillah62@gmail.com>
2025-02-03 23:45:58 -0500

    my approach to leetcode 74

--- binary-search/74-search-a-2d-matrix.py
+++ binary-search/74-search-a-2d-matrix.py
@@ -55,3 +55,63 @@ class Solution:
                 low = mid + 1
 
         return False
+
+
+'''
+Optimal approach:
+    TC: O(log(n*m))
+    SC: O(1)
+
+'''
+
+
+'''
+My solution:
+    TC: O(log(n) + log(m))
+    SC: O(1)
+
+    First locate in which row the target is in using binary search. After location which row, perform
+    a binary search on that row to find the target.
+'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+
+        low = 0
+        high = row - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            
+            # check if the target is in the current row
+            if target >= matrix[mid][0] and target <= matrix[mid][col-1]:
+                return self.binarySearch(matrix[mid], target)
+            
+            # if the target is less than the first element of the current row, then the target is in the 
+            # previous rows
+            elif target < matrix[mid][0]:
+                high = mid - 1
+            
+            # if the target is greater than the last element of the current row, then the target is in the rows
+            # after the current row.
+            else:
+                low = mid + 1
+
+        return False
+
+    def binarySearch(self, arr, target):
+        low = 0
+        high = len(arr)
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return True
+            elif arr[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return False
