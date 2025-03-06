23ebd6b39faa29541f783c6ed51865c9e016c012
abc123 <mohtasimbillah62@gmail.com>
2025-03-06 14:50:09 -0500

    optimal approach for leetcode 142

--- linked-list/142-linked-list-cycle-ii.py
+++ linked-list/142-linked-list-cycle-ii.py
@@ -29,3 +29,39 @@ class Solution:
             
         return None
     
+'''
+Optimal approach:
+    TC: O(n),
+    SC: O(1)
+
+    Again, just like leetcode 141, we are using the slow and fast pointer approach for the 
+    optimal approach. We first find if a cycle exists. Then we find the first node of the cycle
+    by resetting the slow pointer to the head and moving both the slow and fast pointers by one.
+    The point where they meet is the first node of the cycle.
+'''
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+
+        # if we reached the end of the linked list with the slow and fast pointers not being
+        # the same, then we can conclude that there are no cycles and return None
+        if fast is None or fast.next is None:
+            return None
+
+        # But if there is a cycle, the above if statement will not execute and we will then find
+        # the first node of the cycle.
+        slow = head
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow         # or fast, since they are the same
