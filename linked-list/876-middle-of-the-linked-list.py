b1dc723d26f3d62ac8691099eed77dbb2ab3b6b2
abc123 <mohtasimbillah62@gmail.com>
2025-03-06 10:59:41 -0500

    added ListNode class and iterative approach for leetcode 206

--- linked-list/876-middle-of-the-linked-list.py
+++ linked-list/876-middle-of-the-linked-list.py
@@ -8,6 +8,12 @@ Brute Force approach:
     Simply, calculate the size of the linked list first and then find the middle index. Return
     the linked list from that index.
 '''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         n = 0           # size of linked list
