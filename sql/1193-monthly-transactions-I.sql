353942d97d7aeeb0d10fda74ff305e31e1b482a6
Mohtasim Billah <mohtasimbillah62@gmail.com>
2025-11-10 10:07:40 -0500

    leetcode 1193 done

---
+++ sql/1193-monthly-transactions-I.sql
@@ -0,0 +1,18 @@
+-- Question:
+-- Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+-- Return the result table in any order.
+
+-- Approach:
+-- Just follow the question
+
+
+
+SELECT 
+    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
